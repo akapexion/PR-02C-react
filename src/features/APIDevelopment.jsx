9d1b222c07da50321c8c634d28d262ceff5f9e51
Fasahat <aptechfasahat@gmail.com>
2026-04-21 12:59:34 +0500

    ---

--- src/features/APIDevelopment.jsx
+++ src/features/APIDevelopment.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 const APIDevelopment = () => {
   const [toggle, setToggle] = useState(false);
   const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
 
   //   console.log("Outside the UseEffect");
 
@@ -11,7 +12,8 @@ const APIDevelopment = () => {
     const api_base = "https://dummyjson.com/products";
     try {
       const response = await axios.get(api_base);
-      console.log(response);
+      console.log(response.data.products);
+      setData(response.data.products);
     } catch (err) {
       console.log(err);
     }
@@ -27,6 +29,23 @@ const APIDevelopment = () => {
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>Add</button>
       <button onClick={() => setToggle(!toggle)}>Toggle</button>
+
+      <p>Products API Data</p>
+
+      <div className="grid grid-cols-3 place-items-center">
+        {data.map((d) => (
+          <div className="border border-gray-300 shadow-2xl w-[300px]">
+            <div className="banner">
+              <img src={d.images} alt="" />
+            </div>
+
+            <div className="content">
+              <p>{d.title}</p>
+              <p>{d.price}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
